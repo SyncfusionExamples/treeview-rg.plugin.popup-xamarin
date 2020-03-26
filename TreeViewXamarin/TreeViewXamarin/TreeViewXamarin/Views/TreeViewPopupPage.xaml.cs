a37c61fc6309ad044093c1fda43e0590be24c397
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-03-26 20:21:46 +0530

    XAMARIN-34075 - Sample added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Views/TreeViewPopupPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TreeViewXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TreeViewPopupPage : PopupPage
+    {
+        public TreeViewPopupPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
